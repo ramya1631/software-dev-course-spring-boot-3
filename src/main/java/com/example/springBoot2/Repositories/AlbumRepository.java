199e4e5e245ad9f8cf5849dddd3bfbc53783d214
ramya1631 <ramya.koluguri16@gmail.com>
2025-06-22 14:55:36 -0500

    completed tasks

---
+++ src/main/java/com/example/springBoot2/Repositories/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.example.springBoot2.Repositories;
+
+import com.example.springBoot2.models.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album,Integer>{
+
+
+}
